522c0dfceb08d995ab0f6d9ef6757059308cb47e
Cerealizable <alberstc1@gmail.com>
2020-11-11 14:01:43 -0800

    Ironing out the sign up page

--- web/src/views/index.js
+++ web/src/views/index.js
@@ -3,4 +3,5 @@ export { default as Account } from './Account';
 export { default as Ecommerce } from './Ecommerce';
 export { default as NotFound } from './NotFound';
 export { default as NotFoundCover } from './NotFoundCover';
+export { default as SignupCover } from './SignupCover';
 export { default as Startup } from './Startup';
