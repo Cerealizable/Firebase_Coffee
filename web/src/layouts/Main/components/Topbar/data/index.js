c857a0277a4d5759eb733db4df90e44e1ab917b1
Cerealizable <alberstc1@gmail.com>
2020-11-19 10:38:05 -0800

    Created a shoppingCart mock and filled it with static data

---
+++ web/src/layouts/Main/components/Topbar/data/index.js
@@ -0,0 +1,22 @@
+export const cartItems = [
+  {
+    price: 2.60,
+    title: 'Good Life',
+    quantity: 3
+  },
+  {
+    price: 1.59,
+    title: 'Thou mayest',
+    quantity: 5
+  },
+  {
+    price: 3.44,
+    title: 'Oryx Eclipse',
+    quantity: 2
+  },
+  {
+    price: 2.80,
+    title: 'True stone',
+    quantity: 4
+  },
+];
